5fd4b1fc705091a18d889075480caed21c856dce
SofiAS1221 <zoteevAS15@yandex.ru>
2026-03-10 01:15:52 +0300

    docs: замена readme

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
-import {createRouter, createWebHistory} from  'vue-router';
-import Home from '../views/Home.vue';
-import UsersView from '../views/Users.vue';
-import LoginView from '../views/Login.vue';
-import ProfileView from '../views/ProfileView.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import UsersView from '../views/Users.vue'
+import LoginView from '../views/Login.vue'
+import ProfileView from '../views/ProfileView.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/home', component: HomeView },
   { path: '/users', component: UsersView },
-   { path: '/login', component: LoginView },
-   {
+  { path: '/login', component: LoginView },
+  {
     path: '/profile',
     component: ProfileView,
     beforeEnter: (to, from, next) => {
-    const token = localStorege.getItem('token') ;
-if (token) {
-  next('login');
-}else{
-  next();
-}
-   }
-  }
-];
+      const token = localStorege.getItem('token')
+      if (token) {
+        next('login')
+      } else {
+        next()
+      }
+    },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
+<script>
+  { path: '/', redirect: '/home' }
+   </script>
